351cbd3c7f419b1246f85102103d65755af49c94
Julio C. Melchor P <jcmelchorp@gmail.com>
2025-02-02 11:27:07 -0600

    mejorar el componente de landing; agregar nuevos estilos, actualizar animaciones y contenido, y optimizar la estructura del HTML

--- src/app/core/layout/landing/landing.component.ts
+++ src/app/core/layout/landing/landing.component.ts
@@ -1,14 +1,16 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
 import { hueRotateAnimation } from 'angular-animations';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [MatButtonModule,RouterModule],
+  imports: [MatIconModule,MatCardModule,MatButtonModule,RouterModule],
   animations:[
-    hueRotateAnimation({ anchor: 'hueButton', duration: 20000 })
+    hueRotateAnimation({ anchor: 'hueButton', duration: 5000 })
   ],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss',
