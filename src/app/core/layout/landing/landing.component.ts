ad0b8c1c07dc016f0cb3a86385fe3bad460ec84c
Julio C. Melchor P <jcmelchorp@gmail.com>
2025-02-02 09:49:07 -0600

    mejorar el diseño del formulario de inscripción y la página de inicio; agregar animaciones y contenido informativo, ajustar estilos y mejorar la estructura

--- src/app/core/layout/landing/landing.component.ts
+++ src/app/core/layout/landing/landing.component.ts
@@ -1,11 +1,19 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
+import { hueRotateAnimation } from 'angular-animations';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule,RouterModule],
+  animations:[
+    hueRotateAnimation({ anchor: 'hueButton', duration: 20000 })
+  ],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LandingComponent { }
+export class LandingComponent { 
+  hueBtnState = false;
+}
