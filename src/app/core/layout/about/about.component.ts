032bd6ec899dc0da283ef647e412b15cb79eb1da
Julio C. Melchor P <jcmelchorp@gmail.com>
2025-02-15 22:07:07 -0600

    agregar tarjetas de presentación en el componente about y mejorar la estructura del componente faq; ajustar la navegación en el encabezado para dispositivos móviles

--- src/app/core/layout/about/about.component.ts
+++ src/app/core/layout/about/about.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
